5bad9b297576760949a572fea52e18393810bbde
Myles Byrne <myles@myles.id.au>
2012-09-06 17:17:43 -0500

    comment out debugging log statements

--- browser-extensions/chrome/reloader.js
+++ browser-extensions/chrome/reloader.js
@@ -8,12 +8,13 @@ function connect() {
   ws.onmessage = function(event) {
     // todo: do nothing if this "tab" is a dev console?
     var msg = JSON.parse(event.data)
-    console.log(msg)
+    // console.log(msg)
     if (msg.css_only) chrome.tabs.executeScript(null, {file: "reload-css.js"})
     else chrome.tabs.reload()
   }
 
   function reconnect(err) {
+    // console.log("reconnecting")
     if ((Date.now() - lastConnectionAttempt) > 1000) connect()
     else setTimeout(connect, 1000)
   }
